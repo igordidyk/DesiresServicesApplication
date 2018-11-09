9224247addd628e7c03ad59dd4a656c7c1e7e148
Ihor Didyk <ihordidyk95@gmail.com>
2018-11-09 23:13:49 +0200

    dev: initialized project and added basic configuration

---
+++ src/main/java/com/ids/DesireServicesApplication.java
@@ -0,0 +1,12 @@
+package com.ids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesireServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesireServicesApplication.class, args);
+    }
+}
