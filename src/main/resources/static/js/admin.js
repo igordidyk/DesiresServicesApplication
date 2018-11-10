1144524c165dbb4512f5452cfb33f4c4ecd3c2d1
Nadiia Shevtsova <5555577nadia@gmail.com>
2018-11-10 22:55:40 +0200

    web: added menu layer

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -135,3 +135,105 @@ new Lightpick({
     field: document.getElementById('datepicker'),
     singleDate: false
 });
+
+//padding for navigation bar
+let padForMenu = document.querySelectorAll('.item-nav-admin-even');
+for (let i = 0; i < padForMenu.length; i++) {
+    for (let j = 0; j < 9; j++) {
+        padForMenu[i].style.marginLeft = String(j + 2) + 'px';
+        padForMenu[i].style.width = String(j * 2 + 60) + 'px';
+    }
+}
+
+//create elements for menu
+let menuAdministrator = document.querySelectorAll('.grid-menu-administrator');
+for (let i = 0; i < menuAdministrator.length; i++) {
+    let inputDishMenu = document.createElement('input');
+    setAttributes(inputDishMenu, {'class': 'item-menu-for-administrator name-dish', 'id': 'name-dish'});
+
+    let inputDishConsistMenu = document.createElement('input');
+    setAttributes(inputDishConsistMenu, {'class': 'item-menu-for-administrator dish-consist', 'id': 'dish-consist'});
+
+    let inputWeightMenu = document.createElement('input');
+    setAttributes(inputWeightMenu, {'class': 'item-menu-for-administrator weight', 'id': 'weight'});
+
+    let inputPriceMenu = document.createElement('input');
+    setAttributes(inputPriceMenu, {'class': 'item-menu-for-administrator price', 'id': 'price'});
+
+    let btnMenu = document.createElement('button');
+    setAttributes(btnMenu, {'class': 'add-btn-admin', 'disabled': 'disabled'});
+
+    let imgAddMenu = document.createElement('img');
+    setAttributes(imgAddMenu, {'class': 'add-img-admin', 'src': '../static/img/add.png'});
+
+    btnMenu.append(imgAddMenu);
+
+    let gridMenuAdministrator = document.createElement('div');
+    gridMenuAdministrator.setAttribute('class', 'grid-menu-administrator');
+
+    appendChildren(gridMenuAdministrator, {inputDishMenu, inputDishConsistMenu, inputWeightMenu,
+        inputPriceMenu, btnMenu});
+
+    menuAdministrator[i].after(gridMenuAdministrator);
+}
+
+//checking if inputs in menu aren't empty
+let listOfDish = document.querySelectorAll('.list-of-dish');
+let addBtnAdmin = document.querySelectorAll('.add-btn-admin');
+let inputValueNameDish = document.querySelectorAll('.name-dish');
+let inputValueDishConsist = document.querySelectorAll('.dish-consist');
+let inputValueWeight = document.querySelectorAll('.weight');
+let inputValuePrice = document.querySelectorAll('.price');
+for (let i = 0; i < addBtnAdmin.length; i++) {
+    for (let j = 0; j < listOfDish.length; j++) {
+        if (j === i) {
+            inputValueNameDish[j].addEventListener('keyup', function () {
+                inputValueNameDish[j].value !== '' ? addBtnAdmin[i].disabled = false : addBtnAdmin[i].disabled = true;
+            });
+            inputValueDishConsist[j].addEventListener('keyup', function () {
+                inputValueDishConsist[j].value !== '' ? addBtnAdmin[i].disabled = false : addBtnAdmin[i].disabled = true;
+            });
+            inputValueWeight[j].addEventListener('keyup', function () {
+                inputValueWeight[j].value !== '' ? addBtnAdmin[i].disabled = false : addBtnAdmin[i].disabled = true;
+            });
+            inputValuePrice[j].addEventListener('keyup', function () {
+                inputValuePrice[j].value !== '' ? addBtnAdmin[i].disabled = false : addBtnAdmin[i].disabled = true;
+            });
+        }
+    }
+    addBtnAdmin[i].addEventListener('click', function () {
+        for (let j = 0; j < listOfDish.length; j++) {
+            if(j === i){
+                let divEl1 = document.createElement('div');
+                divEl1.appendChild(document.createTextNode(inputValueNameDish[j].value));
+                let divEl2 = document.createElement('div');
+                divEl2.appendChild(document.createTextNode(inputValueDishConsist[j].value));
+                let divEl3 = document.createElement('div');
+                divEl3.appendChild(document.createTextNode(inputValueWeight[j].value));
+                let divEl4 = document.createElement('div');
+                divEl4.appendChild(document.createTextNode(inputValuePrice[j].value));
+
+                let btnDeleteEl = document.createElement('button');
+                btnDeleteEl.setAttribute('class', 'delete-btn-menu');
+                let imgDeleteEl = document.createElement('img');
+                setAttributes(imgDeleteEl, {'src': '../static/img/delete.png', 'class': 'delete-img-menu'});
+                btnDeleteEl.appendChild(imgDeleteEl);
+
+                let commonDiv = document.createElement('div');
+                commonDiv.setAttribute('class', 'grid-menu-administrator');
+                appendChildren(commonDiv, {divEl1, divEl2, divEl3, divEl4, btnDeleteEl});
+                listOfDish[j].after(commonDiv);
+
+                btnDeleteEl.addEventListener('click', function () {
+                    commonDiv.remove();
+                });
+
+                inputValueNameDish[j].value = '';
+                inputValueDishConsist[j].value = '';
+                inputValueWeight[j].value = '';
+                inputValuePrice[j].value = '';
+                addBtnAdmin[i].disabled = true;
+            }
+        }
+    });
+}
