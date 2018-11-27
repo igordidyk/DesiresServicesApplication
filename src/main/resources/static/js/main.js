940aee7457f70b885c00f3ffd068512ea6ad6472
Nadiia Shevtsova <5555577nadia@gmail.com>
2018-11-27 23:29:42 +0200

    dev: refactored code in workers list

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,6 @@
 console.log("include requests js");
 
 let addEmployee = document.querySelector("#addEmployee");
-let getEmployees = document.querySelector("#getAll");
-
 
 let inputOnKey = document.querySelector('#fullName');
 let inputNumber = document.querySelector('#number');
@@ -17,8 +15,9 @@ inputNumber.addEventListener('keyup', function () {
     inputNumber.value !== '' ? addEmployee.disabled = false : addEmployee.disabled = true;
 });
 
-
+let countNumber = 0;
 addEmployee.addEventListener('click', function () {
+    countNumber = 0;
     let employee = { name:inputOnKey.value.split(' ')[0],
         secondName:inputOnKey.value.split(' ')[1],
         phoneNumber:inputNumber.value
@@ -29,57 +28,51 @@ addEmployee.addEventListener('click', function () {
         type:'post',
         data:JSON.stringify(employee),
         contentType:'application/json',
-        success:function () {
+        success:function (data) {
             inputOnKey.value = '';
             inputNumber.value = '';
             addEmployee.disabled = true;
             console.log("Employee "+ employee + " was added");
-        },
+            getListWorker();
+            },
         error:function () {
             console.log("Failed to save");
         }
     });
 });
 
-
-getEmployees.addEventListener('click', function () {
-
+function getListWorker() {
     $.ajax({
         url:'/admin/getEmployees',
         type:'get',
         success:function (data) {
+            for(let emp in data) {
+                if (countNumber === 0) {
+                    let divEl = document.createElement('p');
+                    divEl.setAttribute('class', 'text-workers list-workers-for-remove');
+                    divEl.appendChild(document.createTextNode(data[data.length-1].name + " " + data[data.length-1].secondName));
 
-            for(emp in data){
-                let divEl = document.createElement('p');
-                divEl.setAttribute('class', 'text-workers list-workers-for-remove');
-                divEl.appendChild(document.createTextNode(emp.name + " "+ emp.secondName));
+                    let divElNumber = document.createElement('p');
+                    divElNumber.setAttribute('class', 'text-workers list-workers-for-remove');
+                    divElNumber.appendChild(document.createTextNode(data[data.length-1].phoneNumber));
 
-                let divElNumber = document.createElement('p');
-                divElNumber.setAttribute('class', 'text-workers list-workers-for-remove');
-                divElNumber.appendChild(document.createTextNode(emp.phoneNumber));
+                    let btnDeleteEl = document.createElement('button');
+                    btnDeleteEl.setAttribute('class', 'delete-btn');
 
-                let btnDeleteEl = document.createElement('button');
-                btnDeleteEl.setAttribute('class', 'delete-btn');
-                let imgDeleteEl = document.createElement('img');
-                setAttributes(imgDeleteEl, {'src': '../static/img/delete.png', 'class': 'delete-img'});
-                btnDeleteEl.appendChild(imgDeleteEl);
+                    let commonDiv = document.createElement('div');
+                    commonDiv.setAttribute('class', 'common-div');
+                    appendChildren(commonDiv, {divEl, divElNumber, btnDeleteEl});
 
-                let commonDiv = document.createElement('div');
-                commonDiv.setAttribute('class', 'common-div');
-                appendChildren(commonDiv, {divEl, divElNumber, btnDeleteEl});
-
-                list.appendChild(commonDiv);
-                btnDeleteEl.addEventListener('click', function () {
-                    commonDiv.remove();
-                });
+                    list.appendChild(commonDiv);
+                    btnDeleteEl.addEventListener('click', function () {
+                        commonDiv.remove();
+                    });
+                    countNumber++;
+                }
             }
-
-
         },
         error:function () {
             console.log("Failed to show");
         }
     });
-});
-
-
+}
